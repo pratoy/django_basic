7a01260ac4c41ae5e216d364fd961ea46dbc982d
Pratoy Kumar Proshad <67535048+pratoy@users.noreply.github.com>
2022-03-12 14:33:42 +0600

    Add files via upload

---
+++ searchv1/views.py
@@ -0,0 +1,13 @@
+from re import TEMPLATE
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates")'
+)
+
+def index(request):
+    today = datetime.datetime.now().date()
+    return render(request, "index.html", {"today": today})
